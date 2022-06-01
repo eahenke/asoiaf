c68028d4aa37897207db6b236cc96425ad4f3730
Eric Henke <eahenke@gmail.com>
2022-05-31 17:42:23 -0700

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Books from './components/books';
 import { ApiProvider } from './providers';
@@ -6,7 +7,19 @@ import { ApiProvider } from './providers';
 function App() {
     return (
         <ApiProvider>
-            <Books />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Books />}></Route>
+                    <Route
+                        path="*"
+                        element={
+                            <main>
+                                <p>404</p>
+                            </main>
+                        }
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
         </ApiProvider>
     );
 }
